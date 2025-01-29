e85971f885871842c68a891819af2c85b986f52d
sangeeth-606 <sangeeth999123@gmail.com>
2025-01-29 05:20:21 +0000

    connected to mongodb atlas

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { resolve } = require('path');
+require('dotenv').config();
+const connectDb = require('./db');
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+connectDb();
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
